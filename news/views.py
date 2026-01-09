9019c37af7777f94c4e98962ee3f12de3fc261d0
uzsheruz007 <sherqobilishqobilov009@gmail.com>
2026-01-09 14:44:22 +0500

    Fix import error and update views

--- news/views.py
+++ news/views.py
@@ -19,7 +19,7 @@ class PostListView(ListView):
     model = Post
     template_name = 'post_list.html'
     context_object_name = 'posts'
-    paginate_by = 8
+    paginate_by = 6
     
     def get_queryset(self):
         queryset = Post.objects.filter(status='published')
